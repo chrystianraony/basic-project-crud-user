b24fe2650c01a80c0a890a1fb5f2d88ee7bf4079
schulte <schultev2@gmail.com>
2022-07-05 10:59:37 -0300

    adicionado onclick

--- site/js/medicos/index.js
+++ site/js/medicos/index.js
@@ -1,79 +1,79 @@
-const initFn = (e, options = { }) => {
-    let table = document.querySelector("tbody#tabelaTeste");
+const initFn = (e, options = { nome: 'top' }) => {
+  let table = document.querySelector("tbody#tabelaTeste");
 
-    // capturar os campos de filtros e passar na URL
-    let url = "http://localhost:8080/medicos"
+  // capturar os campos de filtros e passar na URL
+  let url = "http://localhost:8080/medicos";
 
+  keys = Object.keys(options);
+
+  if (keys.length > 0) {
+    url += "?";
+
+    keys.forEach((key) => {
+      url += `${key}=${options[key]}`;
+
+      if (keys.indexOf(key) !== keys.length - 1) {
+        url += "&";
+      }
+    });
+  }
+
+    nomeSelect = document.querySelector("select.nome-select"),
+    crmSelect = document.querySelector("select.crm-select"),
+    especSelect = document.querySelector("select.especializacao-select"),
     
-    keys = Object.keys(options)
 
-    if (keys.length > 0) {
-      url += '?'
+  axios.get(url).then((response) => {
+    medicos = response.data;
+    medicos.forEach((medico) => {
+      var option = document.createElement("option");
+      // nomeSelect.id = "medico_id";
+      nomeSelect.nome = "nome";
+      option.value = `${medico.nome}`;
+      nomeSelect.value = option.value;
+      option.innerHTML = `${medico.nome}`;
+      nomeSelect.appendChild(option);
+    });
 
-      keys.forEach((key) => {
-        url += `${key}=${options[key]}`
+    const eMedico = document.querySelector(".nome-select");
+    new Choices(eMedico);
+  });
 
-        if (keys.indexOf(key) !== keys.length - 1) {
-          url += '&'
-        }
-      })
-    }
-    
-    let nomeSelect = document.querySelector("select.nome-select");
-    let crmSelect = document.querySelector("select.crm-select");
-    let especSelect = document.querySelector("select.especializacao-select");
+  axios.get(url).then((response) => {
+    medicos = response.data;
+    medicos.forEach((medico) => {
+      var option = document.createElement("option");
+      crmSelect.id = "medico_id";
+      option.value = `${medico.id}`;
+      crmSelect.value = option.value;
+      option.innerHTML = `${medico.crm}`;
+      crmSelect.appendChild(option);
+    });
 
-    axios.get(url).then((response) => {
-      medicos = response.data;
-      medicos.forEach((medico) => {
-        var option = document.createElement("option");
-        nomeSelect.id = "medico_id";
-        option.value = `${medico.id}`;
-        nomeSelect.value = option.value;
-        option.innerHTML = `${medico.nome}`;
-        nomeSelect.appendChild(option);
-      });
+    const eMedico = document.querySelector(".crm-select");
+    new Choices(eMedico);
+  });
 
-      const eMedico = document.querySelector(".nome-select");
-      new Choices(eMedico) ;
-    })
+  axios.get(url).then((response) => {
+    medicos = response.data;
+    medicos.forEach((medico) => {
+      var option = document.createElement("option");
+      especSelect.id = "medico_id";
+      option.value = `${medico.id}`;
+      especSelect.value = option.value;
+      option.innerHTML = `${medico.especializacao}`;
+      especSelect.appendChild(option);
+    });
 
-    axios.get(url).then((response) => {
-      medicos = response.data;
-      medicos.forEach((medico) => {
-        var option = document.createElement("option");
-        crmSelect.id = "medico_id";
-        option.value = `${medico.id}`;
-        crmSelect.value = option.value;
-        option.innerHTML = `${medico.crm}`;
-        crmSelect.appendChild(option);
-      });
+    const eMedico = document.querySelector(".especializacao-select");
+    new Choices(eMedico);
+  });
 
-      const eMedico = document.querySelector(".crm-select");
-      new Choices(eMedico) ;
-    })
+  console.log(url);
 
-    axios.get(url).then((response) => {
-      medicos = response.data;
-      medicos.forEach((medico) => {
-        var option = document.createElement("option");
-        especSelect.id = "medico_id";
-        option.value = `${medico.id}`;
-        especSelect.value = option.value;
-        option.innerHTML = `${medico.especializacao}`;
-        especSelect.appendChild(option);
-      });
-
-      const eMedico = document.querySelector(".especializacao-select");
-      new Choices(eMedico) ;
-    })
-
-
-    console.log(url)
-
-    axios.get(url).then((response) => {
-      medicos = response.data;
-      medicos.forEach((medico) => {
+  axios.get(url).then((response) => {
+    medicos = response.data;
+    medicos.forEach((medico) => {
       let tr = document.createElement("tr");
       tr.innerHTML = ` 
             <td>${medico.nome}</td> 
@@ -96,32 +96,38 @@ const initFn = (e, options = { }) => {
 
       table.appendChild(tr);
     });
-});
+  });
+
 };
 
-function excluirMedico(event, id) {
-    console.log(event);
-    event.preventDefault();
-  
-    Swal.fire({
-      title: "Deletar Médico?",
-      showDenyButton: true,
-      showCancelButton: false,
-      confirmButtonText: "Excluir",
-      denyButtonText: `Cancelar`,
-    }).then((result) => {
-      if (result.isConfirmed) {
-        axios
-          .delete(`http://localhost:8080/medicos/${id}`)
-          .then((data) => {
-            Swal.fire("Excluido!", "", "success").then((result) => {
-              window.location = "/medicos/";
-            });
-          })
-          .catch((error) => {
-            console.log("Error:", error);
-          });
-      }
-    });
+function filtrarMedico(event) {
+  options = {
+
   }
-  
+}
+
+function excluirMedico(event, id) {
+  console.log(event);
+  event.preventDefault();
+
+  Swal.fire({
+    title: "Deletar Médico?",
+    showDenyButton: true,
+    showCancelButton: false,
+    confirmButtonText: "Excluir",
+    denyButtonText: `Cancelar`,
+  }).then((result) => {
+    if (result.isConfirmed) {
+      axios
+        .delete(`http://localhost:8080/medicos/${id}`)
+        .then((data) => {
+          Swal.fire("Excluido!", "", "success").then((result) => {
+            window.location = "/medicos/";
+          });
+        })
+        .catch((error) => {
+          console.log("Error:", error);
+        });
+    }
+  });
+}
